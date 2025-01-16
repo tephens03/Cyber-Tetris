99af6e446e0b4d04e3314a693735e7643ced3ee4
tephens03 <sttr2130@gmail.com>
2025-01-16 10:43:46 -0500

    Update Readme

--- src/main/BackgroundImage.java
+++ src/main/BackgroundImage.java
@@ -4,7 +4,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class BackgroundImage extends JLabel {
-    final private String BACKGROUND_IMAGE_URL = "/Users/transtephen/Desktop/Projects/Tetris/background.gif";
+    final private String BACKGROUND_IMAGE_URL = "../asset/background.gif";
 
     public BackgroundImage() {
         ImageIcon bgImage = new ImageIcon(BACKGROUND_IMAGE_URL);
